85c0d1d44fe79030b7fceba50513fdda148e9b20
Szymon Niewczas <szymon_niewczas@wp.pl>
2020-07-12 14:08:03 +0200

    added config template and tests

---
+++ src/tests/XmlTemplate.test.ts
@@ -0,0 +1,26 @@
+import { Template } from "../model/XmlConfigTemplate";
+
+describe("Xml parser and template tests", () => {
+  const parser = new DOMParser();
+  const document = parser.parseFromString(Template, "text/xml");
+  test("Should parse string template to valid xml document", () => {
+    expect(document).not.toBeNull();
+  });
+
+  test("Should have root element name HALNyUniversalProvisioning", () => {
+    expect(document.documentElement.nodeName).toEqual(
+      "HALNyUniversalProvisioning"
+    );
+  });
+
+  test("Should have node with name HALNyConfiguration", () => {
+    const result = document.evaluate(
+      "//HALNyUniversalProvisioning/HALNyConfiguration",
+      document,
+      null,
+      XPathResult.FIRST_ORDERED_NODE_TYPE,
+      null
+    );
+    expect(result.singleNodeValue.nodeName).toEqual("HALNyConfiguration");
+  });
+});
