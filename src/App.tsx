04c2cfb8edb3544301efb30f85b5bf6dfb89f4e9
Szymon Niewczas <szymon_niewczas@wp.pl>
2020-07-12 11:09:48 +0200

    file creation works

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,22 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import logo from "./logo.svg";
+import "./App.css";
+import Button from "@material-ui/core/Button";
+import { saveAs } from "file-saver";
 
 function App() {
+  const saveTestFile = () => {
+    var file = new File(["Hello, world!"], "TestFile.txt", {
+      type: "text/plain;charset=utf-8"
+    });
+    saveAs(file);
+  };
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Button variant="contained" color="primary" onClick={saveTestFile}>
+        Create test file
+      </Button>
     </div>
   );
 }
