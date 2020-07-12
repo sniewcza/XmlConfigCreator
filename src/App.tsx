23a1c62a57760617e1d5d5e47340259901ce3730
Szymon Niewczas <szymon_niewczas@wp.pl>
2020-07-12 16:57:45 +0200

    remove unused icons

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,16 @@
 import React from "react";
-import logo from "./logo.svg";
 import "./App.css";
 import Button from "@material-ui/core/Button";
 import { saveAs } from "file-saver";
+import { Template } from "./model/XmlConfigTemplate";
 
+const parser = new DOMParser();
+const xmlserialzier = new XMLSerializer();
 function App() {
   const saveTestFile = () => {
-    var file = new File(["Hello, world!"], "TestFile.txt", {
+    const document = parser.parseFromString(Template, "text/xml");
+    const documentString = xmlserialzier.serializeToString(document);
+    const file = new File([documentString], "TestFile.xml", {
       type: "text/plain;charset=utf-8"
     });
     saveAs(file);
